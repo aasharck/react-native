b9995faac850e5c6f1672fd6700156f6519a45e8
Aashar ck <aasharck@outlook.com>
2023-07-27 10:03:28 +0300

    updated

---
+++ jobs-app/constants/index.js
@@ -0,0 +1,5 @@
+import images from "./images.js";
+import icons from "./icons.js";
+import { COLORS, FONT, SIZES, SHADOWS } from "./theme.js";
+
+export { images, icons, COLORS, FONT, SIZES, SHADOWS };
